1bb3b7640b593e38b33c1403062e5eaca0afb118
kavit-bokarvadiya <kavitit2000@gmail.com>
2025-04-17 16:24:41 +0530

    first commit

---
+++ basics/basicOutput.rb
@@ -0,0 +1,15 @@
+# Print Output – (puts ya print)
+
+# puts:
+# ------ 
+# Output ke baad ek new line dalta hai.
+
+
+# print:
+# ------
+#  Output ke baad new line nahi dalta.
+
+
+# program :: 1 :: puts and print
+
+print "print kabhi out put ke bad new line me nahi jata"
